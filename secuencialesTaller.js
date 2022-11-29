2a4440622d66e711d566884c084d97b818431aa7
Danielasalazaranaya <slalzardaniela05@gmail.com>
2022-11-29 14:05:25 -0500

    second commit

---
+++ secuencialesTaller.js
@@ -0,0 +1,17 @@
+// ejercicio 1-a
+// let A=2, B=5, C=1
+// total1=3*A-4*B/A**2
+// console.log(total1)
+
+// ejercicio 1-b
+// let A=2, B=5, C=1
+// total2=(((B+C)/2*A+10)*3*B)-6
+// console.log(total2)
+
+// ejercicio 3
+// let total, capital=0
+// capital=parseFloat(prompt('dijite la capital que desea invertir'))
+// total=capital-1.2
+// console.log(`Por invertir en el banco usted ganara ${total}`)
+
+let salario=980000, comision=170000 valorDeVenta
